d21040a0c9524e4a1d1c71e780c671b6bcab7479
Irene Ng <irene@Irenes-MacBook-Pro.local>
2015-04-28 18:57:04 -0700

    SSI works

---
+++ about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <!-- This is all the metadata required for a website-->
+    <meta title="description" content="description">
+    <meta title="contents" content="contents">
+    <meta name="viewport" content="width=device-width" initial-scale="1.0">
+    <link rel="stylesheet" type="text/css" href="boba_style.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+    <title>Moba Boba</title>
+</head>
+
+<body>
+    <?php include 'include/nav.php';?>
+    <div id="main">
+        <div id="story2" class="test-box">
+            <h2 id="story">Our Story</h2>
+            <p id="description">We are two UCI students who were first exposed to the greatness that is boba upon entering as freshmen. Once we had tasted its bobalicious-ness, our cravings cannot be satisfied purely by Ring Road's 2 dollar boba. Communicating with other students, we were met with similar responses - we would like to have boba anytime we desire; thus, we have decided to launch Moba Boba as the solution to the nationally growing thirst for boba. Let no one in the nation ever be without boba!</p>
+            <br/>
+            <h2 id="story">The thirst is real!</h2>
+        </div>
+        <img id="aboutImage" src="images/aboutus.jpg" />
+    </div>
+
+</body>
+
+</html>
--- locations.php
+++ locations.php
@@ -14,48 +14,7 @@
 </head>
 
 <body>
-    <nav id="nav-bar">
-    <a href="boba.html" title="">
-        <img id="logo" src="images/moba.png" alt="">
-    </a>
-    <span id="moba"><!-- Moba Boba --></span>
-    <ul>
-        <li class="nav-link"><a href="menu.html">Menu</a></li>
-        <li class="nav-link"><a href="locations.html">Locations</a></li>
-        <li class="nav-link"><a href="about.html">About</a></li>
-    </ul>
-</nav>
-<div class="container">
-        <div id="side">
-            <h2 id="test" class="test-box">Testimonies</h2>
-            <ul>
-                <li class="test-box">
-                    <p>These were some really amazing drinks. The fact that they deliver is probably the single best thing ever thought about. I will buy my drinks from here. Forever. Even after I die. Yes, seriously.
-                        <br /><br /> -Irene Ng</p>
-                </li>
-                <li class="test-box">
-                    <blockquote cite="">
-                        <p>The website is kinda lame but I enjoyed their services. The one dude who coded this site really needs to stop being so immature.
-                            <br /><br /> -Gabriel Blanco</p>
-                    </blockquote>
-                </li>
-                <li class="test-box">
-                    <blockquote cite="">
-                        <p>$2 boba???? PSHHH!!!!!!!! FREE DELIVERY BOBA. NO MORE YELLING YOUR FACE OFF AT RING ROAD FOR DRINKS!
-                            <br /><br /> -Annoyed Guy that walks on Ring Road</p>
-                    </blockquote>
-                </li>
-                <li class="test-box">
-                    <blockquote cite="">
-                        <p>It's aight.
-                            <br /><br />-Barack Bobama</p>
-                    </blockquote>
-                </li>
-            </ul>
-        </div>
-    </div>
-
-    <!--#include virtual="nav.html" -->
+    <?php include 'include/nav.php';?>
     <div id="main">
         <div id="content">
             <h1 id="location">Locations</h1>
